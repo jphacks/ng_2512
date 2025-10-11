66e16772c5e9a96b696788066c03a333fb8a0df5
michi0008 <michi322418@gmail.com>
2025-10-11 15:29:42 +0900

    feat: FL21タスクの実装

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,3 +1,11 @@
-from .app import create_app
+from typing import Any
+
+
+def create_app(*args: Any, **kwargs: Any):
+    """Proxy to lazily import the Flask application factory."""
+
+    from .app import create_app as _create_app
+
+    return _create_app(*args, **kwargs)
 
 __all__ = ["create_app"]
